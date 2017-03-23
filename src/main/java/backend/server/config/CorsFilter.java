68dcef7fc2a1d6d1c0e5c4c744d9c47c2df09a3b
Makarand Salvi <makarand.salvi@neptune-int.com>
2017-03-23 18:04:09 +0530

    headers

--- src/main/java/backend/server/config/CorsFilter.java
+++ src/main/java/backend/server/config/CorsFilter.java
@@ -18,16 +18,13 @@ public class CorsFilter extends OncePerRequestFilter
     {
         System.out.println(request.getHeader(ORIGIN));
         System.out.println(request.getMethod());
-        if (request.getHeader(ORIGIN).equals("null"))
-        {
-            String origin = request.getHeader(ORIGIN);
+        String origin = request.getHeader(ORIGIN);
 
-            response.setHeader("Access-Control-Allow-Origin", origin);
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-            response.setHeader("Access-Control-Allow-Headers",
-                    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        }
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         if (request.getMethod().equals("OPTIONS"))
         {
             try
