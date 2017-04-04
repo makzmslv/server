263a8c7097282782c65ec1fd0a8b82d2e1dc8e0f
Makarand Salvi <makarand.salvi@neptune-int.com>
2017-04-04 16:57:48 +0530

    added customer details to order

--- src/main/resources/static/frontend/js/controllers/homeController.js
+++ src/main/resources/static/frontend/js/controllers/homeController.js
@@ -5,54 +5,58 @@ var homeController = angular.module('homeController', [ 'ngRoute' ]).controller(
         $scope.searchParameters = {};
         $scope.screenOptions = {};
         var initPage;
-        
+
         $scope.getHotels = function () {
-        	var queryParams = $scope.searchParameters;
-        	HotelService.getAllHotels(queryParams).$promise.then(function(result) {
+            var queryParams = $scope.searchParameters;
+            HotelService.getAllHotels(queryParams).$promise.then(function(result) {
                 $scope.hotels = result;
             });
         };
-        
+
         $scope.getOrders = function (hotel) {
-        	HotelService.getOrdersForHotel(hotel.id).$promise.then(function(result) {
-        		$scope.orders = result;
-        		$scope.screenOptions.showHotels = false;
-        		$scope.screenOptions.selectedHotel = hotel;
-        		$scope.screenOptions.showOrders = true;
-        		$scope.screenOptions.showOrderDetails = false;
+            HotelService.getOrdersForHotel(hotel.id).$promise.then(function(result) {
+                $scope.orders = result;
+                $scope.screenOptions.showHotels = false;
+                $scope.screenOptions.selectedHotel = hotel;
+                $scope.screenOptions.showOrders = true;
+                $scope.screenOptions.showOrderDetails = false;
             });
         };
-        
+
         $scope.getOrderDetails = function (order) {
-        	HotelService.getOrderDetails(order.id).$promise.then(function(result) {
-        		$scope.orderDetails = result;
-        		$scope.screenOptions.showHotels = false;
-        		$scope.screenOptions.showOrders = false;
-        		$scope.screenOptions.showOrderDetails = true;
-        		$scope.screenOptions.selectedOrder = order;
+            HotelService.getOrderDetails(order.id).$promise.then(function(result) {
+                $scope.orderDetails = result;
+                $scope.screenOptions.showHotels = false;
+                $scope.screenOptions.showOrders = false;
+                $scope.screenOptions.showOrderDetails = true;
+                $scope.screenOptions.selectedOrder = order;
+                $scope.totalCost = 0;
+                $scope.orderDetails.forEach(function (entry) {
+                    $scope.totalCost = $scope.totalCost + entry.cost;
+                });
             });
         };
-        
+
         initPage = function () {
-        	$scope.screenOptions.showHotels = true;
-    		$scope.screenOptions.showOrders = false;
-    		$scope.screenOptions.showOrderDetails = false;
-    		$scope.screenOptions.selectedHotel = null;
+            $scope.screenOptions.showHotels = true;
+            $scope.screenOptions.showOrders = false;
+            $scope.screenOptions.showOrderDetails = false;
+            $scope.screenOptions.selectedHotel = null;
         };
-        
+
         $scope.goBacktoHotels = function () {
-        	$scope.screenOptions.showHotels = true;
-    		$scope.screenOptions.showOrders = false;
-    		$scope.screenOptions.showOrderDetails = false;
-    		$scope.screenOptions.selectedHotel = null;
+            $scope.screenOptions.showHotels = true;
+            $scope.screenOptions.showOrders = false;
+            $scope.screenOptions.showOrderDetails = false;
+            $scope.screenOptions.selectedHotel = null;
         };
-        
+
        $scope.goBacktoOrders = function () {
-        	$scope.screenOptions.showHotels = false;
-    		$scope.screenOptions.showOrders = true;
-    		$scope.screenOptions.showOrderDetails = false;
+            $scope.screenOptions.showHotels = false;
+            $scope.screenOptions.showOrders = true;
+            $scope.screenOptions.showOrderDetails = false;
         };
-        
+
         initPage();
     }
 
