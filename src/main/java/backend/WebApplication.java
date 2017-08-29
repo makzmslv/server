6922ae18f71e23cd5b5c38ba257ff2d5b4d178ed
Makarand Salvi <makarand.salvi@neptune-int.com>
2017-08-29 10:39:56 +0530

    fixed deployment errors

--- src/main/java/backend/WebApplication.java
+++ src/main/java/backend/WebApplication.java
@@ -1,20 +1,22 @@
 package backend;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.dozer.DozerBeanMapper;
-import org.dozer.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class WebApplication
+public class WebApplication extends SpringBootServletInitializer
 {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(WebApplication.class);
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(WebApplication.class, args);
